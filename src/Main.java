856660c82b758e15237d581a1d338ed1521e99bc
Sgt. Stromboli <dandale1997@aol.com>
2019-09-26 15:14:54 -0400

    Update Main.java
    Update 9/26/2019

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
+/*	Name: Daniel V Dalessandro
+ * 	Description: This is the Main file class, and does nothing too special
+ * 		*****DO NOT MAKE THIS FILE A CLUTTERED MESS*****
+ * 	Input: None. Only a starter file.
+ * 	Output: Main file runs all other files project files.
+ * 
+ * 	Version: 0.1
+ * 	Last updated: 9/26/2019
+ * 		Description of update:
+ * 			- Added Main method and a simple output to command line to verify that java was indeed working on machine.
+ * 			- Commented what needed to be done to file
+ * 
+ */
 
-public class Main {
+/* IMPORT LIBRARIES */
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+public class Main		// Main class to run all other .java files
+{
+	/* DATA FIELDS */
+	
+	public static void main(String[] args)
+	{
+		
 	}
 
 }
